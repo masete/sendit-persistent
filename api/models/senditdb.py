de52152f811b3a9cbdbcb10b0ab2474953545769
masete <nicholasmasete72@gmail.com>
2018-11-17 15:32:29 +0300

    [Delivers #162030565]
    Add the database

---
+++ api/models/senditdb.py
@@ -0,0 +1,39 @@
+import psycopg2
+
+
+class DatabaseConnection:
+    def __init__(self):
+        self.commands = (
+            """
+            CREATE TABLE IF NOT EXISTS users(
+                    user_id SERIAL PRIMARY KEY,
+                    username VARCHAR(50) NOT NULL,
+                    email VARCHAR(50) NOT NULL,
+                    password VARCHAR(200) NOT NULL
+                    )
+            
+            """,
+            """
+                 CREATE TABLE IF NOT EXISTS parcel (                    
+                    parcel_id SERIAL PRIMARY KEY,
+                    user_id INT REFERENCES users(user_id),
+                    parcel_location VARCHAR(20) NOT NULL,
+                    parcel_destination VARCHAR(20) NOT NULL,
+                    parcel_weight INTEGER NOT NULL, 
+                    parcel_description VARCHAR(20) NOT NULL
+                                                 
+                    
+                )
+            """
+        )
+
+        self.connection = psycopg2.connect(dbname='senditdb',
+                                           user='postgres',
+                                           password='postgres',
+                                           host='localhost',
+                                           port='5432')
+        self.connection.autocommit = True
+        self.cursor = self.connection.cursor()
+        print(self.cursor)
+        for command in self.commands:
+            self.cursor.execute(command)
